7fbfc695cfe6b70391545b30109a3960e82ba6c9
sylvaindecout <sylvain.decout+github@gmail.com>
2024-12-07 20:23:45 +0100

    :wastebasket: Deprecate code relying on JPA

--- java/src/main/java/fr/sdecout/handson/persistence/book/BookRepository.java
+++ java/src/main/java/fr/sdecout/handson/persistence/book/BookRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+/** @deprecated Replace with jOOQ */
+@Deprecated
 public interface BookRepository extends JpaRepository<BookEntity, String> {
     List<BookEntity> findByTitleLikeIgnoringCase(String hint);
 }
--- java/src/main/java/fr/sdecout/handson/persistence/library/LibraryRepository.java
+++ java/src/main/java/fr/sdecout/handson/persistence/library/LibraryRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
+/** @deprecated Replace with jOOQ */
+@Deprecated
 public interface LibraryRepository extends JpaRepository<LibraryEntity, String> {
 
     List<LibraryEntity> findByAddress_postalCodeStartingWith(String departmentId);
