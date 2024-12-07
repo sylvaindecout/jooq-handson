de497568ad7395af7c467d830f349a12945e59c7
sylvaindecout <sylvain.decout+github@gmail.com>
2024-12-07 20:32:03 +0100

    :bulb: Add instructions

--- java/src/main/java/fr/sdecout/handson/rest/shared/AddressField.java
+++ java/src/main/java/fr/sdecout/handson/rest/shared/AddressField.java
@@ -2,6 +2,33 @@ package fr.sdecout.handson.rest.shared;
 
 import jakarta.validation.constraints.NotBlank;
 
+/**
+ * <h1>TODO: Step 8</h1>
+ * <p>
+ * While all RDBMS come with standard SQL, they also introduce specifics.
+ * For instance, <a href="https://www.postgresql.org">PostgreSQL</a> introduces a JSONB type that is more performant
+ * than native JSON and has specific operators and indexing capabilities.
+ * </p>
+ * <p>
+ * Uncomment <code>004.add-library-address-as-jsonb.yaml</code> migration script in
+ * <code>db.changelog-master.yaml</code> in order to add a JSONB field.
+ * This breaks the build!
+ * The reason lies in the process used to generate jOOQ sources (cf. <code>source-generation.md</code>), which uses an
+ * <a href="https://h2database.com">in-memory database</a>.
+ * </p>
+ * <p>
+ * Replacing it with the actual RDBMS is still possible by running a dedicated container, which using
+ * <a href="https://testcontainers.com">Testcontainers</a> makes way easier.
+ * Unfortunately, the official Maven / Gradle plugins do not include this feature.
+ * However, it can be integrated into the build specifically.
+ * Still, it can be noted that
+ * <a href="https://github.com/testcontainers/testcontainers-jooq-codegen-maven-plugin">a dedicated Maven plugin</a>
+ * was recently released to address this specific problematic.
+ * </p>
+ *
+ * @see <a href="https://blog.jooq.org/using-testcontainers-to-generate-jooq-code/">Using Testcontainers to Generate jOOQ Code</a>
+ * @see <a href="https://www.jooq.org/doc/latest/manual/sql-building/column-expressions/json-functions/">JSON functions</a>
+ */
 public record AddressField(
         @NotBlank String line1,
         String line2,
--- kotlin/README.md
+++ kotlin/README.md
@@ -10,6 +10,17 @@
 * Populate test DB: `docker exec -i jooq-handson-database psql postgresql://user:user123@localhost:5432/handson < ../database/init_data.sql`
 * Use REST API: [OpenAPI specification](../openapi.yml)
 
+# Steps
+
+1. [Basic commands](src/main/kotlin/fr/sdecout/handson/persistence/library/DbLibraryAdapter.kt)
+2. [Queries from single tables](src/main/kotlin/fr/sdecout/handson/persistence/library/DbLibraryAdapter.kt)
+3. [Queries from several tables](src/main/kotlin/fr/sdecout/handson/persistence/library/DbLibraryAdapter.kt)
+4. [Nested queries](src/main/kotlin/fr/sdecout/handson/persistence/book/DbBookAdapter.kt)
+5. [Code generation](src/main/resources/db/changelog/db.changelog-master.yaml)
+6. [Type converters](src/main/kotlin/fr/sdecout/handson/persistence/converters/IsbnConverter.kt)
+7. [Batch operations](src/main/kotlin/fr/sdecout/handson/persistence/book/DbBookAdapter.kt)
+8. [Supporting specifics with Testcontainers](src/main/kotlin/fr/sdecout/handson/rest/shared/AddressField.kt)
+
 ## Context
 
 More info in [parent directory](../README.md).
