d9e48f568117bc9951760799c192a82ddf9999e4
sylvaindecout <sylvain.decout+github@gmail.com>
2024-12-14 12:54:32 +0100

    :construction: wip

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,10 +1,34 @@
 import nu.studer.gradle.jooq.JooqEdition
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jooq.meta.jaxb.ForcedType
 import org.jooq.meta.jaxb.Logging
 import org.jooq.meta.jaxb.Nullability.NOT_NULL
 import org.jooq.meta.jaxb.Property
+import org.jooq.codegen.GenerationTool
+import org.jooq.meta.jaxb.Configuration
+import org.jooq.meta.jaxb.Database
+import org.jooq.meta.jaxb.Generator
+import org.jooq.meta.jaxb.Jdbc
+import org.jooq.meta.jaxb.Target
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.utility.MountableFile
+
+var myUrl = ""
+var myUser = ""
+var myPassword = ""
+
+buildscript {
+  repositories {
+    mavenCentral()
+  }
+  dependencies {
+    classpath("org.postgresql:postgresql:42.5.0")
+    classpath("org.testcontainers:postgresql:1.17.6")
+    classpath("org.jooq:jooq-codegen:3.18.7")
+  }
+}
 
 val archunitVersion = "1.3.0"
 val jooqLiquibaseVersion = "3.19.10"
@@ -80,15 +104,22 @@ jooq {
 
       jooqConfiguration.apply {
         logging = Logging.WARN
+        jdbc.apply {
+          url = myUrl // ${jooq.codegen.jdbc.url}
+          user = myUser // ${jooq.codegen.jdbc.username}
+          password = myPassword // ${jooq.codegen.jdbc.password}
+        }
         generator.apply {
           name = "org.jooq.codegen.KotlinGenerator"
           database.apply {
-            name = "org.jooq.meta.extensions.liquibase.LiquibaseDatabase"
-            withProperties(
-              Property().withKey("rootPath").withValue("$projectDir/src/main/resources"),
-              Property().withKey("scripts").withValue("/db/changelog/db.changelog-master.yaml"),
-              Property().withKey("includeLiquibaseTables").withValue("false")
-            )
+            excludes = "DATABASECHANGELOG | DATABASECHANGELOGLOCK"
+            inputSchema = "public"
+            // name = "org.jooq.meta.extensions.liquibase.LiquibaseDatabase"
+//            withProperties(
+//              Property().withKey("rootPath").withValue("$projectDir/src/main/resources"),
+//              Property().withKey("scripts").withValue("/db/changelog/db.changelog-master.yaml"),
+//              Property().withKey("includeLiquibaseTables").withValue("false")
+//            )
             withForcedTypes(
               ForcedType()
                 .withUserType("fr.sdecout.handson.rest.shared.Isbn")
@@ -109,5 +140,44 @@ jooq {
         }
       }
     }
+  }
+}
+
+tasks.withType<KotlinCompile> {
+  dependsOn("jooqCodegen")
+}
+
+tasks.register("jooqCodegen") {
+  doLast {
+    val schemaPath = layout.projectDirectory.file("/src/main/resources/schema.sql").asFile.path
+    val containerInstance = PostgreSQLContainer<Nothing>("postgres:16.1")
+      .apply {
+        withCopyToContainer(MountableFile.forHostPath(schemaPath), "/docker-entrypoint-initdb.d/init.sql")
+        start()
+      }
+
+    myUrl = containerInstance.jdbcUrl
+    myUser = containerInstance.username
+    myPassword = containerInstance.password
+
+    Configuration()
+      .withJdbc(
+        Jdbc()
+          .withDriver("org.postgresql.Driver")
+          .withUrl(containerInstance.jdbcUrl)
+          .withUser(containerInstance.username)
+          .withPassword(containerInstance.password)
+      )
+      .withGenerator(
+        Generator()
+          .withDatabase(Database().withInputSchema("public"))
+          .withTarget(
+            Target()
+              .withPackageName("org.jooq.generated")
+              .withDirectory("${layout.projectDirectory}/src/main/java")
+          )
+      ).also(GenerationTool::generate)
+
+    containerInstance.stop()
   }
 }
