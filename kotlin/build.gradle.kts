a79c320743f6e150281bb33b4eff32f8aa55063d
sylvaindecout <sylvain.decout+github@gmail.com>
2024-12-14 22:01:23 +0100

    :construction: worked again!

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,15 +1,6 @@
-import liquibase.Contexts
-import liquibase.LabelExpression
-import liquibase.Liquibase
-import liquibase.integration.commandline.CommandLineUtils
-import liquibase.resource.CompositeResourceAccessor
-import liquibase.resource.FileSystemResourceAccessor
-import liquibase.resource.ResourceAccessor
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0
 import org.jooq.meta.jaxb.Logging
-import java.util.*
-
 
 val archunitVersion = "1.3.0"
 val jooqLiquibaseVersion = "3.19.10"
@@ -46,18 +37,12 @@ dependencies {
   runtimeOnly("org.postgresql", "postgresql")
   implementation("org.liquibase", "liquibase-core")
 
-  // jooqCodegen("org.jooq", "jooq-meta-extensions-liquibase", jooqLiquibaseVersion)
-  // jooqCodegen("org.liquibase", "liquibase-core")
-
   liquibaseRuntime("org.liquibase", "liquibase-core")
   liquibaseRuntime ("info.picocli", "picocli", "4.7.6") // FIXME: variabilize version
   liquibaseRuntime("org.postgresql", "postgresql")
-  // liquibaseRuntime ("org.liquibase", "liquibase-groovy-dsl", "3.0.2") // FIXME: remove?
 
   jooqCodegen("org.postgresql", "postgresql")
   jooqCodegen("org.testcontainers", "postgresql")
-//  jooqCodegen("org.jooq","jooq-codegen", dependencyManagement.importedProperties["jooq.version"])
-//  jooqCodegen("org.jooq","jooq-meta-extensions", dependencyManagement.importedProperties["jooq.version"])
 
   testImplementation("org.springframework.boot", "spring-boot-starter-test") {
     exclude("org.junit.vintage", "junit-vintage-engine")
@@ -75,9 +60,9 @@ buildscript {
   }
   dependencies {
     classpath("org.testcontainers", "postgresql", "1.20.4") // FIXME
-    classpath("org.liquibase", "liquibase-gradle-plugin", "3.0.1") {
-      exclude(group="org.liquibase", module="liquibase-core")
-    }
+//    classpath("org.liquibase", "liquibase-gradle-plugin", "3.0.1") {
+//      exclude(group="org.liquibase", module="liquibase-core")
+//    }
     classpath("org.liquibase", "liquibase-core", "4.29.2")
   }
 }
@@ -130,50 +115,16 @@ jooq {
 }
 
 liquibase {
-//  activities {
-//    closureOf<NamedDomainObjectContainer<Activity>> {
-//      create("main", closureOf<Activity> {
-//        arguments = mapOf(
-//          "driver" to "org.h2.Driver",
-//          "changeLogFile" to "src/main/resources/db/changelog.xml",
-//          "url" to "jdbc:h2:mem:test",
-//          "logLevel" to "debug"
-//        )
-//      })
-//    }
-//  }
   activities.register("main") {
       val allArgs = mapOf<String, String>(
         "changelogFile" to "src/main/resources/db/changelog/db.changelog-master.yaml",
         "driver" to "org.postgresql.Driver",
         "logLevel" to "debug",
-        "url" to "jdbc:postgresql://localhost:5432/handson",
-        "user" to "user",
-        "username" to "user",
-        "password" to "user123",
-
-
-//        "url" to System.getProperty("jooq.codegen.jdbc.url"),
-//        "user" to System.getProperty("jooq.codegen.jdbc.username"),
-//        "password" to System.getProperty("jooq.codegen.jdbc.password"),
-//        "changeLogDirectory" to "${buildFile.parent}/src/main/resources", // ${projectDir}
-//        "changelog-directory" to "${buildFile.parent}/src/main/resources", // ${projectDir}
-//        "searchPath" to "src/main/resources",
-//        "search-path" to "src/main/resources",
-//        "changeLogFile" to "/db/changelog/db.changelog-master.yaml",
-//        "liquibase.command.update.changelogFile" to "/db/changelog/db.changelog-master.yaml",
-//        "liquibase.command.changelogFile" to "/db/changelog/db.changelog-master.yaml",
-//        "changelog-file" to "/db/changelog/db.changelog-master.yaml",
-//        "--changelog-file" to "/db/changelog/db.changelog-master.yaml",
-//        "--changelog-file" to "/db/changelog/db.changelog-master.yaml",
-//        "logLevel" to "debug",
-//        "help" to "",
-//        "changelogFile" to "src/main/resources/db/changelog/db.changelog-master.yaml",
-        )
-//      println(">>> this is me! " + allArgs.toList().joinToString(separator = "\n"))
-//      changelogParameters = allArgs
+        "url" to System.getProperty("jooq.codegen.jdbc.url"),
+        "username" to System.getProperty("jooq.codegen.jdbc.username"),
+        "password" to System.getProperty("jooq.codegen.jdbc.password"),
+      )
       arguments = allArgs
-    // runList = "main" // FIXME: unnecessary?
   }
 }
 
@@ -181,18 +132,8 @@ tasks.named("compileKotlin") {
   dependsOn(tasks.named("jooqCodegen"))
 }
 
-//tasks.named("update") {
-//  doFirst {
-//    println(">>> update > Starting")
-//  }
-//  doLast {
-//    println(">>> update > Disposing")
-//  }
-//}
-
 tasks.register("startDatabase") {
   doLast {
-    println(">>> startDatabase > Starting testcontainer")
     org.testcontainers.containers.PostgreSQLContainer("postgres:15")
       .withDatabaseName("handson")
       .withUsername("user")
@@ -206,52 +147,17 @@ tasks.register("startDatabase") {
         System.setProperty("testcontainer.imageName", dockerImageName)
       }
   }
-  finalizedBy("update") // liquibase
+  finalizedBy("migrateDatabaseSchema")
 }
 
-//tasks.register("migrateSchema") {
-//  doLast {
-//    this.url = url
-//    this.username = user
-//    this.password = password
-//    this.driver = "" //your driver
-//    this.outputFile = "" // The path in which the script have to be flushed.
-//    val fsOpener: FileSystemResourceAccessor = liquibase.resource.FileSystemResourceAccessor()
-//    val clOpener: CommandLineResourceAccessor = CommandLineResourceAccessor(javaClass.classLoader)
-//    val fileOpener: CompositeResourceAccessor =
-//      liquibase.resource.CompositeResourceAccessor(*kotlin.arrayOf<ResourceAccessor>(fsOpener, clOpener))
-//
-//    val database: Database = CommandLineUtils.createDatabaseObject(
-//      fileOpener,
-//      this.url,
-//      this.username,
-//      this.password,
-//      this.driver,
-//      this.defaultCatalogName,
-//      this.defaultSchemaName,
-//      this.outputDefaultCatalog.toBoolean(),
-//      this.outputDefaultSchema.toBoolean(),
-//      this.databaseClass,
-//      this.driverPropertiesFile,
-//      this.propertyProviderClass,
-//      this.liquibaseCatalogName,
-//      this.liquibaseSchemaName,
-//      this.databaseChangeLogTableName,
-//      this.databaseChangeLogLockTableName
-//    )
-//
-//
-//    val liquibase: Liquibase = liquibase.Liquibase(d, null, database)
-//    liquibase.update(liquibase.Contexts(this.contexts), liquibase.LabelExpression(this.labels), getOutputWriter())
-//
-//  }
-//}
+tasks.register("migrateDatabaseSchema") {
+  dependsOn("update")
+}
 
 tasks.register("stopDatabase") {
   doLast {
     val containerId = System.getProperty("testcontainer.containerid")
     val imageName = System.getProperty("testcontainer.imageName")
-    println(">>> stopDatabase > Stopping testcontainer:  $containerId - $imageName")
     org.testcontainers.utility.ResourceReaper
       .instance()
       .stopAndRemoveContainer(containerId, imageName);
@@ -262,26 +168,3 @@ tasks.named("jooqCodegen") {
   dependsOn("startDatabase")
   finalizedBy("stopDatabase")
 }
-
-/*
-            <plugin>
-                <groupId>org.liquibase</groupId>
-                <artifactId>liquibase-maven-plugin</artifactId>
-                <executions>
-                    <execution>
-                        <id>init-db</id>
-                        <phase>generate-sources</phase>
-                        <goals>
-                            <goal>update</goal>
-                        </goals>
-                        <configuration>
-                            <url>${jooq.codegen.jdbc.url}</url>
-                            <username>${jooq.codegen.jdbc.username}</username>
-                            <password>${jooq.codegen.jdbc.password}</password>
-                            <changeLogDirectory>${basedir}/src/main/resources</changeLogDirectory>
-                            <changeLogFile>/db/changelog/db.changelog-main.yaml</changeLogFile>
-                        </configuration>
-                    </execution>
-                </executions>
-            </plugin>
- */
