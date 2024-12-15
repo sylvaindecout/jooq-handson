f8752c5eef26d0ce5b701a038221fbb1c9227c71
sylvaindecout <sylvain.decout+github@gmail.com>
2024-12-15 11:29:13 +0100

    :heavy_plus_sign: Integrate jOOQ

--- java/src/main/java/fr/sdecout/handson/AppConfig.java
+++ java/src/main/java/fr/sdecout/handson/AppConfig.java
@@ -1,7 +1,17 @@
 package fr.sdecout.handson;
 
+import org.springframework.boot.autoconfigure.jooq.DefaultConfigurationCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static org.jooq.conf.RenderNameCase.LOWER;
 
 @Configuration
 public class AppConfig {
+
+    @Bean
+    DefaultConfigurationCustomizer configurationCustomizer() {
+        return config -> config.settings().withRenderNameCase(LOWER);
+    }
+
 }
