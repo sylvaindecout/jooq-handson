f4688df5884481d1edf51947d6bb5a6f7fb1646b
sylvaindecout <sylvain.decout+github@gmail.com>
2025-02-18 16:11:05 +0100

    :construction: steps 1 to 5

--- kotlin/src/main/kotlin/fr/sdecout/handson/persistence/library/DbLibraryAdapter.kt
+++ kotlin/src/main/kotlin/fr/sdecout/handson/persistence/library/DbLibraryAdapter.kt
@@ -1,12 +1,18 @@
 package fr.sdecout.handson.persistence.library
 
 import fr.sdecout.handson.persistence.book.BookRepository
+import fr.sdecout.handson.persistence.jooq.tables.references.BOOK_AUTHOR
+import fr.sdecout.handson.persistence.jooq.tables.references.LIBRARY
+import fr.sdecout.handson.persistence.jooq.tables.references.LIBRARY_BOOK
 import fr.sdecout.handson.rest.author.AuthorId
 import fr.sdecout.handson.rest.library.*
 import fr.sdecout.handson.rest.shared.AddressField
 import fr.sdecout.handson.rest.shared.Isbn
+import fr.sdecout.handson.rest.shared.LibraryField
 import jakarta.transaction.Transactional
-import org.springframework.data.repository.findByIdOrNull
+import org.jooq.DSLContext
+import org.jooq.impl.DSL.countDistinct
+import org.jooq.kotlin.fetchSingleValue
 import org.springframework.stereotype.Component
 
 @Component
@@ -14,6 +20,7 @@ import org.springframework.stereotype.Component
 class DbLibraryAdapter(
     private val libraryRepository: LibraryRepository,
     private val bookRepository: BookRepository,
+    private val dsl: DSLContext,
 ) : LibraryAccess, LibrarySearch, LibraryCreation, BookCollectionUpdate {
 
     /**
@@ -32,14 +39,15 @@ class DbLibraryAdapter(
      *
      * @see <a href="https://www.jooq.org/doc/latest/manual/sql-building/sql-statements/insert-statement/">The INSERT statement</a>
      */
-    override fun addLibrary(name: String, address: AddressField): LibraryId = LibraryId.next().also {
-        libraryRepository.save(
-            LibraryEntity(
-                id = it.value,
-                name = name,
-                address = address.toAddressEntity(),
-            )
-        )
+    override fun addLibrary(name: String, address: AddressField): LibraryId = LibraryId.next().also { id ->
+        dsl.insertInto(LIBRARY)
+            .set(LIBRARY.ID, id.value)
+            .set(LIBRARY.NAME, name)
+            .set(LIBRARY.ADDRESS_LINE_1, address.line1)
+            .set(LIBRARY.ADDRESS_LINE_2, address.line2)
+            .set(LIBRARY.POSTAL_CODE, address.postalCode)
+            .set(LIBRARY.CITY, address.city)
+            .execute()
     }
 
     /**
@@ -48,9 +56,11 @@ class DbLibraryAdapter(
      * @see addLibrary
      */
     override fun addBook(libraryId: LibraryId, isbn: Isbn) {
-        libraryRepository.getReferenceById(libraryId.value)
-            .apply { bookRepository.getReferenceById(isbn.compressedValue).let { availableBooks?.add(it) } }
-            .let { libraryRepository.save(it) }
+        dsl.insertInto(LIBRARY_BOOK)
+            .set(LIBRARY_BOOK.BOOK, isbn.compressedValue)
+            .set(LIBRARY_BOOK.LIBRARY, libraryId.value)
+            .onDuplicateKeyIgnore()
+            .execute()
     }
 
     /**
@@ -63,17 +73,28 @@ class DbLibraryAdapter(
      * @see <a href="https://www.jooq.org/doc/latest/manual/sql-building/sql-statements/select-statement/#select-from-single-tables">SELECT from single tables</a>
      * @see <a href="https://www.jooq.org/doc/latest/manual/sql-execution/fetching/">Fetching</a>
      */
-    override fun findLibrary(id: LibraryId): LibraryResponse? = libraryRepository.findByIdOrNull(id.value)
-        ?.toLibraryResponse()
+    override fun findLibrary(id: LibraryId): LibraryResponse? =  dsl
+        .selectFrom(LIBRARY)
+        .where(LIBRARY.ID.equal(id.value))
+        .fetchAny { LibraryResponse(
+            id = it.id,
+            name = it.name,
+            address = AddressField(it.addressLine_1, it.addressLine_2, it.postalCode, it.city),
+        ) }
 
     /**
      * # TODO: Step 2
      *
      * @see findLibrary
      */
-    override fun searchLibrariesClosestTo(postalCode: PostalCode): List<LibrarySearchResponseItem> = libraryRepository
-        .findByAddress_postalCodeStartingWith(postalCode.departmentCode)
-        .map { it.toLibrarySearchResponseItem() }
+    override fun searchLibrariesClosestTo(postalCode: PostalCode): List<LibrarySearchResponseItem> = dsl
+        .selectFrom(LIBRARY)
+        .where(LIBRARY.POSTAL_CODE.startsWithIgnoreCase(postalCode.departmentCode))
+        .fetch { LibrarySearchResponseItem(
+            id = it.id,
+            name = it.name,
+            address = AddressField(it.addressLine_1, it.addressLine_2, it.postalCode, it.city),
+        ) }
 
     /**
      * # TODO: Step 3
@@ -83,9 +104,22 @@ class DbLibraryAdapter(
      *
      * @see <a href="https://www.jooq.org/doc/latest/manual/sql-building/sql-statements/select-statement/#select-from-a-complex-table-expression">SELECT from a complex table expression</a>
      */
-    override fun searchLibrariesWithBookAvailable(isbn: Isbn): List<LibrarySearchResponseItem> = libraryRepository
-        .findByavailableBooks_isbn(isbn.compressedValue)
-        .map { it.toLibrarySearchResponseItem() }
+    override fun searchLibrariesWithBookAvailable(isbn: Isbn): List<LibrarySearchResponseItem> = dsl
+        .select()
+        .from(LIBRARY_BOOK).join(LIBRARY).on(LIBRARY.ID.equal(LIBRARY_BOOK.LIBRARY))
+        .where(LIBRARY_BOOK.BOOK.equal(isbn.compressedValue))
+        .fetch {
+            LibrarySearchResponseItem(
+                id = it.get(LIBRARY.ID),
+                name = it.get(LIBRARY.NAME),
+                address = AddressField(
+                    it.get(LIBRARY.ADDRESS_LINE_1),
+                    it.get(LIBRARY.ADDRESS_LINE_2),
+                    it.get(LIBRARY.POSTAL_CODE),
+                    it.get(LIBRARY.CITY)
+                ),
+            )
+        }
 
     /**
      * # TODO: Step 3
@@ -93,6 +127,10 @@ class DbLibraryAdapter(
      * @see searchLibrariesWithBookAvailable
      */
     override fun countLibrariesWithBooksBy(author: AuthorId): Long =
-        libraryRepository.countLibrariesWithBooksBy(author.value)
+        dsl.select(countDistinct(LIBRARY_BOOK.LIBRARY))
+            .from(LIBRARY_BOOK)
+            .join(BOOK_AUTHOR).on(BOOK_AUTHOR.BOOK.equal(LIBRARY_BOOK.BOOK))
+            .where(BOOK_AUTHOR.AUTHOR.equal(author.value))
+            .fetchSingleValue().toLong()
 
 }
