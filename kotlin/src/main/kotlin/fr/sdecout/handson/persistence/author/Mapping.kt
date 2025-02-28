a508e5632138acf8268c28c3f3fd84cb1ae18bde
sylvaindecout <sylvain.decout+github@gmail.com>
2025-02-28 18:25:36 +0100

    :recycle: Simplify Jackson DTOs (Kotlin)

--- kotlin/src/main/kotlin/fr/sdecout/handson/persistence/author/Mapping.kt
+++ kotlin/src/main/kotlin/fr/sdecout/handson/persistence/author/Mapping.kt
@@ -3,7 +3,6 @@ package fr.sdecout.handson.persistence.author
 import fr.sdecout.handson.persistence.book.toBookField
 import fr.sdecout.handson.rest.author.AuthorResponse
 import fr.sdecout.handson.rest.author.AuthorSearchResponseItem
-import fr.sdecout.handson.rest.shared.AuthorField
 
 fun AuthorEntity.toAuthorResponse() = AuthorResponse(
     id = this.id,
@@ -13,10 +12,6 @@ fun AuthorEntity.toAuthorResponse() = AuthorResponse(
 )
 
 fun AuthorEntity.toAuthorSearchResponseItem() = AuthorSearchResponseItem(
-    author = this.toAuthorField()
-)
-
-private fun AuthorEntity.toAuthorField() = AuthorField(
     id = this.id,
     lastName = this.lastName,
     firstName = this.firstName,
--- kotlin/src/main/kotlin/fr/sdecout/handson/rest/library/LibrarySearchResponseItem.kt
+++ kotlin/src/main/kotlin/fr/sdecout/handson/rest/library/LibrarySearchResponseItem.kt
@@ -1,10 +1,12 @@
 package fr.sdecout.handson.rest.library
 
-import com.fasterxml.jackson.annotation.JsonUnwrapped
-import fr.sdecout.handson.rest.shared.LibraryField
+import fr.sdecout.handson.rest.shared.AddressField
 import jakarta.validation.Valid
+import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 
 data class LibrarySearchResponseItem(
-    @field:JsonUnwrapped @field:NotNull @field:Valid val library: LibraryField?,
+    @field:NotBlank val id: String?,
+    @field:NotBlank val name: String?,
+    @field:NotNull @field:Valid val address: AddressField?,
 )
